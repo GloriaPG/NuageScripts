cc4e6232239392e5004341cd0e1025fa6335a4f6
GloriaPG <ing.gloriapalmagonzalez@gmail.com>
2017-06-01 02:27:09 +0000

    The End - Blur

--- sentinella_probestats_filter_by_date.py
+++ sentinella_probestats_filter_by_date.py
@@ -4,31 +4,8 @@ from elasticsearch import Elasticsearch
 client = Elasticsearch(['192.168.0.24:9200'])
 
 SourceNSG = "nsg-branch1-2"
-SrcUplink = "port1"
-"""
-response = client.search(
-    index="nuage_dpi_probestats",
-    body={
-	    "query": {
-	        "range" : {
-	            "timestamp" : {
-	                "gte" : "now-5m"
-	            }
-	        },
-	        "constant_score" : {
-	            "filter" : {
-	                 "bool" : {
-	                    "must" : [
-	                        { "term" : { "SourceNSG" : "{0}".format(SourceNSG) } }, 
-	                        { "term" : { "SrcUplink" : "{0}".format(SrcUplink) } } 
-	                    ]
-	                }
-	            }
-            }
-	    }
-    }
-)
-"""
+SrcUplink = "port2"
+
 response = client.search(
     index="nuage_dpi_probestats",
     body={

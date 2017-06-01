9b7a42cea754fa3f861076a52f62b154ec151b44
GloriaPG <ing.gloriapalmagonzalez@gmail.com>
2017-05-31 22:11:01 -0500

    Meh

---
+++ sentinella_probestats_top10.py
@@ -0,0 +1,28 @@
+import json
+from elasticsearch import Elasticsearch
+
+client = Elasticsearch(['192.168.0.24:9200'])
+
+SourceNSG = "nsg-branch1-2"
+SrcUplink = "port1"
+
+response = client.search(
+    index="nuage_dpi_probestats",
+    body={
+	  "query": {
+	    "filtered": {
+	      "from" : 0, "size" : 10,
+	      "filter" : {
+	            "bool" : {
+	                "must" : [
+	                    { "term" : { "SourceNSG" : "{0}".format(SourceNSG) } }, 
+	                    { "term" : { "SrcUplink" : "{0}".format(SrcUplink) } } 
+	                ]
+	            }
+	        }
+	    }
+	  }
+	}
+)
+
+print json.dumps(response, indent=4, sort_keys=True)

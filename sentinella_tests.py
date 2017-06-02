c1379f52f347d3cdb10e37739dc0c990a51e3ef5
GloriaPG <ing.gloriapalmagonzalez@gmail.com>
2017-06-02 14:10:43 -0500

    Create response to detail branch

--- sentinella_tests.py
+++ sentinella_tests.py
@@ -14,8 +14,15 @@ import logging
 from vspk.utils import set_log_level
 
 # Auth vars
+"""
 vsd_ip = '147.75.69.37'
 api_url = "https://vsd1.sdn40r8.lab:8443"
+username = "csproot"
+password = "csproot"
+enterprise = "csp"
+"""
+vsd_ip = '192.168.0.20'
+api_url = "https://192.168.0.20:8443"
 username = "csproot"
 password = "csproot"
 enterprise = "csp"
@@ -71,12 +78,63 @@ def vsc_health(csproot):
                         else:
                             break
 
+def cafectavba(csproot):
+    #print csproot.__dict__
+    response = {}
+    for enterprise in csproot.enterprises.get(filter='ID is "6e51eafc-a2d7-4f6b-9a34-bb5262b60688"'):
+        response['enterprise'] = enterprise
+        nsg_branch_up = 0
+        nsg_branch_dow = 0
+        nsgs = []
+
+        for g in enterprise.ns_redundant_gateway_groups.get(filter='ID is "79ad6616-3e75-4d98-b80f-efea2997d17a"'):
+            for p in g.ns_gateways.get():
+                if p.bootstrap_status != "ACTIVE":
+                    nsg_branch_dow = nsg_branch_dow + 1
+                else:
+                    nsg_branch_up = nsg_branch_up +1
+                ports_data = []
+                for np in p.ns_ports.get():
+                    nsg_port = {
+                        "id": np.id,
+                        "name": np.name,
+                        "description" : np.description,
+                        "status" : np.status,
+                        "nsg-brach" : p.name,
+                        "nsg-branch-id" : p.id
+                    }
+                    #print nsg_port
+                    ports_data.append(nsg_port)
+                    for stats in np.statistics.get():
+                        print stats.stats_data +"\n"
+                        print stats.number_of_data_points + "\n"
+
+                nsg = {
+                    "name": p.name,
+                    "status" : p.bootstrap_status,
+                    "cpu_usage" : 10.0,
+                    "memory_usage" : 10.0,
+                    "ports" : ports_data
+                }
+                print nsg
+                nsgs.append(nsg)
+        
+        response['nsg-branch-dow'] = nsg_branch_dow
+        response['nsg-branch-up'] = nsg_branch_up
+    """
+    for vsp in obj:
+        print "\n\n**********  VSP: %s **********" % vsp.name
+        print vsp.to_dict()
+    """
+    
+            
 def main():
 
     setup_logging()
     csproot = start_csproot_session()
     #import pdb; pdb.set_trace()
-    vsc_health(csproot)
+    cafectavba(csproot)
+    #vsc_health(csproot)
 
 if __name__ == "__main__":
    main()
